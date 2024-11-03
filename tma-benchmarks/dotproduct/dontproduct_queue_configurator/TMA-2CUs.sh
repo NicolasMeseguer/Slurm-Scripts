093d1dbcdba3723ccd91d3a8ddade2017efd57b0
NicolasMeseguer <n.mesegueriborra@um.es>
2024-11-03 18:52:16 +0000

    Implemented the QCU for the Dotproduct.

---
+++ tma-benchmarks/dotproduct/dontproduct_queue_configurator/TMA-2CUs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+##############################
+#       Job blueprint        #
+##############################
+
+# Give your job a name, so you can recognize it in the queue overview
+#SBATCH --job-name=tma_dotproduct_queue_configurator_2cus
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+
+# You may not place any commands before the last SBATCH directive
+
+############################
+#       Actual work        #
+############################
+
+cd /home/nmeseguer/mgpusim/samples/tma
+
+# Dotproduct - Queue
+cd ./dotproduct_queue_configurator
+
+go build
+
+./dotproduct_queue_configurator -timing -report-all -magic-memory-copy -metric-file-name="slurm-2cus" -length=524288 -globalsize=1152 -localsize=576
+
+# Finish the script
+exit 0
