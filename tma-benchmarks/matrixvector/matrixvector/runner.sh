d27a5aaee0e5e0295ae371f7cbc20d8dc636971e
NicolasMeseguer <n.mesegueriborra@um.es>
2025-01-20 21:49:50 +0000

    Added the matrix vector

---
+++ tma-benchmarks/matrixvector/matrixvector/runner.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -l
+
+##############################
+#       Job blueprint        #
+##############################
+
+# Give your job a name, so you can recognize it in the queue overview
+#SBATCH --job-name=tma_matrixvector
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+# You may not place any commands before the last SBATCH directive
+
+############################
+#       Actual work        #
+############################
+
+cd /home/nmeseguer/mgpusim/samples/tma
+
+# Queue Tester
+cd ./matrixvector
+
+go build
+
+LOCALSIZE=576
+DIM_M=2048
+DIM_K=2048
+
+for CU in 1 2 4 5 8 16 32 64 128; do
+
+  # If the iteration is bigger than 5 and the $1 is 'radeon530', then exit
+  if [ $CU -gt 5 ] && [ "$1" == "radeon530" ]; then
+    exit 0
+
+  # If the iteration is bigger than 64 and the $1 is 'r9nano', then exit
+  elif [ $CU -gt 64 ] && [ "$1" == "r9nano" ]; then
+    exit 0
+  
+  # If the iteration is bigger than 128 and the $1 is 'mi100', then exit
+  elif [ $CU -gt 128 ] && [ "$1" == "mi100" ]; then
+    exit 0
+  fi
+
+  GLOBALSIZE=$(( $LOCALSIZE * $CU ))
+
+  for TILESIZE in 64 128 256 512; do
+
+    ./matrixvector -timing -report-all -magic-memory-copy -metric-file-name="slurm-${CU}cu-${TILESIZE}" -dim_m=$DIM_M -dim_k=$DIM_K -TileSizeK=$TILESIZE -globalsize=$GLOBALSIZE -localsize=$LOCALSIZE -gpu-model="$1"
+
+  done
+done
+
+# Create the $1 directory if it does not exist
+mkdir -p $1
+
+# Move the csv files to the $1 directory
+mv slurm-* $1
+
+# Finish the script
+exit 0
--- tma-benchmarks/matrixvector/tma_queue_configurator_driver.sh
+++ tma-benchmarks/matrixvector/tma_queue_configurator_driver.sh
@@ -9,17 +9,11 @@ MODEL_NAME=$1
 
 # If the name of the model is not 'r9nano' or 'mi100', then exit
 if [ "$MODEL_NAME" != "r9nano" ] && [ "$MODEL_NAME" != "mi100" ] && [ "$MODEL_NAME" != "radeon530" ]; then
-
     echo "Invalid model name"
     exit 1
 fi
 
-sbatch TMA-1CU.sh $MODEL_NAME
-sbatch TMA-8CUs.sh $MODEL_NAME
-
-sbatch TMA-16CUs.sh $MODEL_NAME
-sbatch TMA-32CUs.sh $MODEL_NAME
-sbatch TMA-64CUs.sh $MODEL_NAME
+sbatch runner.sh $MODEL_NAME
 
 # Finish the script
 exit 0
