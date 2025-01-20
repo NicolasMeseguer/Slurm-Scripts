ea34adc566c8c4b07b8d0ba96e254be9412a4a7f
NicolasMeseguer <n.mesegueriborra@um.es>
2025-01-20 20:39:01 +0000

    Removed the queue configurator baseline

--- tma-benchmarks/elementwiseK/tma_queue_configurator_driver.sh
+++ tma-benchmarks/elementwiseK/tma_queue_configurator_driver.sh
@@ -9,7 +9,6 @@ MODEL_NAME=$1
 
 # If the name of the model is not 'r9nano' or 'mi100', then exit
 if [ "$MODEL_NAME" != "r9nano" ] && [ "$MODEL_NAME" != "mi100" ] && [ "$MODEL_NAME" != "radeon530" ]; then
-
     echo "Invalid model name"
     exit 1
 fi
