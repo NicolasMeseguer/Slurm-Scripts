40fac08568bc33db8e644b6c3d701c15c116bfb3
NicolasMeseguer <n.mesegueriborra@um.es>
2025-01-20 20:29:30 +0000

    Updated the files to use a loop

--- tma-benchmarks/elementwiseK/notma.sh
+++ tma-benchmarks/elementwiseK/notma.sh
@@ -9,18 +9,11 @@ MODEL_NAME=$1
 
 # If the name of the model is not 'r9nano' or 'mi100', then exit
 if [ "$MODEL_NAME" != "r9nano" ] && [ "$MODEL_NAME" != "mi100" ] && [ "$MODEL_NAME" != "radeon530" ]; then
-
     echo "Invalid model name"
     exit 1
 fi
 
-sbatch NOTMA-1CU.sh $MODEL_NAME
-sbatch NOTMA-2CUs.sh $MODEL_NAME
-sbatch NOTMA-4CUs.sh $MODEL_NAME
-sbatch NOTMA-8CUs.sh $MODEL_NAME
-sbatch NOTMA-16CUs.sh $MODEL_NAME
-sbatch NOTMA-32CUs.sh $MODEL_NAME
-sbatch NOTMA-64CUs.sh $MODEL_NAME
+sbatch runner.sh $MODEL_NAME
 
 # Finish the script
 exit 0
--- tma-benchmarks/elementwiseK/tma_queue.sh
+++ tma-benchmarks/elementwiseK/tma_queue.sh
@@ -9,31 +9,11 @@ MODEL_NAME=$1
 
 # If the name of the model is not 'r9nano' or 'mi100', then exit
 if [ "$MODEL_NAME" != "r9nano" ] && [ "$MODEL_NAME" != "mi100" ] && [ "$MODEL_NAME" != "radeon530" ]; then
-
     echo "Invalid model name"
     exit 1
 fi
 
-sbatch TMA-1CU-1QT.sh $MODEL_NAME
-sbatch TMA-1CU-2QT.sh $MODEL_NAME
-sbatch TMA-1CU-4QT.sh $MODEL_NAME
-sbatch TMA-2CU-1QT.sh $MODEL_NAME
-sbatch TMA-2CU-2QT.sh $MODEL_NAME
-sbatch TMA-2CU-4QT.sh $MODEL_NAME
-sbatch TMA-4CU-1QT.sh $MODEL_NAME
-sbatch TMA-4CU-2QT.sh $MODEL_NAME
-sbatch TMA-4CU-4QT.sh $MODEL_NAME
-sbatch TMA-8CU-1QT.sh $MODEL_NAME
-sbatch TMA-8CU-2QT.sh $MODEL_NAME
-sbatch TMA-8CU-4QT.sh $MODEL_NAME
-sbatch TMA-16CU-1QT.sh $MODEL_NAME
-sbatch TMA-16CU-2QT.sh $MODEL_NAME
-sbatch TMA-32CU-1QT.sh $MODEL_NAME
-sbatch TMA-32CU-2QT.sh $MODEL_NAME
-sbatch TMA-64CU-1QT.sh $MODEL_NAME
-sbatch TMA-64CU-2QT.sh $MODEL_NAME
-
-
+sbatch runner.sh $MODEL_NAME
 
 # Finish the script
 exit 0
